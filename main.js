398693e951e7c14c1eebf4e4aa27103ebc7479a3
Evalife2151 <vi2151eva@gmail.com>
2021-11-07 12:50:28 +0200

    added pwa functionality

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', async () => {
   if (navigator.serviceWorker) {
     try {
-      const reg = await navigator.serviceWorker.register('/sw.js')
+      const reg = await navigator.serviceWorker.register('./sw.js')
     } catch (e) {
       console.log ('error:', e)
     }   
