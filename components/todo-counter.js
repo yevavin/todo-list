7a2ecea4e59336a834d54d9a5bcfd4a1020ca96d
Yeva Vinnitskaya <vi2151eva@gmail.com>
2021-11-24 21:03:34 +0200

    fixed label for even number

--- components/todo-counter.js
+++ components/todo-counter.js
@@ -3,7 +3,10 @@ import todos from '../models/todos.js'
 const todoListCounter = document.querySelector('#todoList .todo-list__footer-counter');
 
 todoListCounter.update = function() {
-  this.innerText = `${todos.filter(item => !item.checked).length} item left`;
+  const number = todos.filter(item => !item.checked).length;
+  this.innerText = (number == 1)
+  ? `${number} item left`
+  : `${number} items left`
 }
 
 todoListCounter.update();
