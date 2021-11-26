1fc65f59504f2c9d32875558b249b18ee5c023b5
Yeva Vinnitskaya <vi2151eva@gmail.com>
2021-11-26 10:24:54 +0200

    getting data from db for an authorized user

--- auth.js
+++ auth.js
@@ -1,4 +1,6 @@
 import userStore from './models/user.js'
+import todos from './models/todos.js'
+import todoList from './components/todo-list.js';
 // import saveTodoToDB from './db.js';
 
 const signInWithGoogle = document.getElementById('signInWithGoogle');
@@ -19,7 +21,13 @@ function signOut() {
 
 firebase.auth().onAuthStateChanged((user) => {
   userStore.user = user;
+  if (user) {
+    const API_PATH = `${user.uid}.json`;
+    todos.load(API_PATH)
+    .then(() => todoList.render())
+  }
 })
+
 firebase.auth().getRedirectResult().then((result) => {
   userStore.user = result.user;
 })
--- models/todos.js
+++ models/todos.js
@@ -1,5 +1,23 @@
 
-const todos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+let todos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+
+import userStore from "./user.js";
+
+const API_URL = 'https://todo-list-app-43909-default-rtdb.firebaseio.com/todos/';
+
+todos.load = async (path) => {
+  return fetch(
+    API_URL + path,
+    { method: 'GET' }
+  )
+    .then(response => response.json())
+    .then((data) => Object.values(data))
+    .then((data) => {
+      console.log(data)
+      todos.splice(0)
+      todos.push(...data)
+    })
+}
 
 todos.addTodo = (todo) => {
   todos.push({
@@ -28,6 +46,23 @@ todos.toggle = (id) => {
 
 const saveTodos = () => {
   localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+todos.saveTodosToDatabase = (API_PATH) => {
+  let todosFromLocalStorage = JSON.parse(localStorage.getItem('todos'));
+  todosFromLocalStorage = todosFromLocalStorage.reduce(function (acc, item, index) {
+    acc[index] = item;
+    return acc;
+  }, {})
+  try {
+    fetch(API_URL + API_PATH, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(todosFromLocalStorage)
+    });
+  } catch (ex) {
+    console.error('ex:', ex);
+  }
 }
 
 export default todos;
